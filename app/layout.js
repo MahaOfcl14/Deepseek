a6684c625511699a807c10d4cdae594a6969b072
MahaOfcl14 <itzmahaarun@gmail.com>
2025-03-30 13:00:58 +0530

    Cleared eeverything, font updated, logo updated

---
+++ app/layout.js
@@ -0,0 +1,25 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inders",
+  subsets: ["latin"],
+});
+
+
+export const metadata = {
+  title: "DeepSeek",
+  description: "Full Stack Project",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
